782022d107c1ca544718bee58b8ee4a4d84c8d72
viljamilves <viljamilves2000@gmail.com>
2021-10-24 21:38:50 +0300

    js stuff

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-$(function(){
-	$.get('data.json', function(json_object) {
-	for (object of json_object) {
-		if (object.image == null) {
-			let div = $('<div class = noImagePost>')
-			}
-		else {
-			let div = $('<div class = ImagePost>')
-		}
+$(document).ready(function() {
+
+	$.get('https://api.npoint.io/1af4e09636416934aacb', function(json_object) {
+		for (obj of json_object) {
+
+			let div = $('<div class = "post1">');
+			let upperdiv = $('<div class = "upper">');
+			let middlediv = $('<div class = "middle">');
+			let bottomdiv = $('<div class = "bottom">');
+			let pdate = $('<p>').text(obj.year);
+			let ptext = $('<p>').text(obj.body);
+
+			upperdiv.append(pdate);
+			middlediv.append(ptext);
+
+			div.append(upperdiv);
+			div.append(middlediv);
+			div.append(bottomdiv);
+
+			$('body').append(div);
 		}
 	});
-	
 })
-	
