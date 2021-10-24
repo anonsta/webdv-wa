9d3665bfc04c1134eac7fe2128800ef9a0e6d6ac
viljamilves <viljamilves2000@gmail.com>
2021-10-24 22:30:37 +0300

    fixed like button location

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ $(document).ready(function() {
 			let bottomdiv = $('<div class = "text">');
 			let pdate = $('<p>').text(obj.year);
 			let ptext = $('<p>').text(obj.body);
+			let pfp = $('<img src="https://www.vhv.rs/dpng/d/436-4363443_view-user-icon-png-font-awesome-user-circle.png" alt="user">');
+			let image = $('<img id=imageid src="" alt="">');
+			$('#imageid').attr('src', obj.image);
+			let likeimage = $('<img src="https://icon-library.com/images/like-button-icon/like-button-icon-3.jpg">');
 
+			console.log(obj.image);
+
+			upperdiv.append(pfp);
 			upperdiv.append(pdate);
 			bottomdiv.append(ptext);
+			bottomdiv.append(likeimage);
 
 			div.append(upperdiv);
 			div.append(middlediv);
