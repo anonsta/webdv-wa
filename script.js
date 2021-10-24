af55ac1be54767d283f1c39070c79f80bc964336
viljamilves <viljamilves2000@gmail.com>
2021-10-24 23:30:22 +0300

    small changes with images

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$.get('https://api.npoint.io/588c784a618918a72553', function(json_object) {
+	$.getJSON('https://api.npoint.io/588c784a618918a72553', function(json_object) {
 		for (obj of json_object) {
 
 			let div = $('<div class = "post1">');
@@ -11,7 +11,9 @@ $(document).ready(function() {
 			let ptext = $('<p>').text(obj.body);
 			let pfp = $('<img src="https://www.vhv.rs/dpng/d/436-4363443_view-user-icon-png-font-awesome-user-circle.png" alt="user">');
 			let image = document.createElement('img');
-			image.src = obj.image;
+			if (obj.image !== null) {
+				image.src = obj.image;
+			}
 			let likeimage = $('<img src="https://icon-library.com/images/like-button-icon/like-button-icon-3.jpg">');
 
 			upperdiv.append(pfp);
