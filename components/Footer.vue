5c68251d70fd51361514fdcda21fb4b96afebc13
Mari <mari@Maris-Air.lan>
2021-11-21 21:39:47 +0200

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-
+<div class="footer-container">
+<div class="footer-text">
+  <a href="webdv-wa/contatcts.html">Contact us</a>
+</div>
+</div>
 </template>
 
 <script>
@@ -9,5 +13,19 @@ name: "Footer"
 </script>
 
 <style scoped>
+
+.footer-container {
+  height: 100px;
+  width: 100%;
+  color: #1e90ff;
+}
+
+.footer-text a {
+  text-align: center;
+  font-size: 18px;
+  color: white;
+}
+
+
 
 </style>
