e5c0971212ccac6e53c15d550a9d550962b0a993
Mari <mari@Maris-Air.lan>
2021-11-21 20:41:26 +0200

    add search on header

--- components/header.vue
+++ components/header.vue
@@ -1,7 +1,14 @@
 <template>
   <header>
+    <nav>
     <div>
         <img src="/images/postit.png" alt="postit">
     </div>
+    <div>
+      <div class="search-container">
+        <input type="text"><button>Search</button>
+      </div>
+    </div>
+    </nav>
   </header>
 </template>
